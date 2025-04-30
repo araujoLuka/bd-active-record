fe0687f8266bc700fd70a386c105e327bd995aff
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2025-04-30 20:13:29 -0300

    feat: add more creators for other tables

---
+++ createEmployeesProjects.rb
@@ -0,0 +1,25 @@
+# Require necessary gems
+require "rubygems"
+require "active_record"
+
+# Establish connection to the database
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/active_record.sqlite3"
+)
+
+# Define constants
+TABLE_EMPLOYEES_PROJECTS = :employees_projects
+
+# Create the 'employees_projects' join table
+begin
+  ActiveRecord::Base.connection.create_table TABLE_EMPLOYEES_PROJECTS do |t|
+    t.references :employee, null: false, foreign_key: true
+    t.references :project, null: false, foreign_key: true
+  end
+  puts "Table '#{TABLE_EMPLOYEES_PROJECTS}' created with columns: employee_id, project_id."
+rescue ActiveRecord::StatementInvalid
+  puts "[create_table] Table '#{TABLE_EMPLOYEES_PROJECTS}' already exists."
+rescue ActiveRecord::ActiveRecordError => e
+  puts "[create_table] Error occurred while creating table: #{e.message}"
+end
---
+++ createEnterprises.rb
@@ -0,0 +1,27 @@
+# Require necessary gems
+require "rubygems"
+require "active_record"
+
+# Define constants
+TABLE_ENTERPRISES = :enterprises
+
+# Establish connection to the database
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/active_record.sqlite3"
+)
+
+# Create the 'enterprises' table
+begin
+  ActiveRecord::Base.connection.create_table TABLE_ENTERPRISES do |t|
+    t.string :name
+    t.string :location
+    t.float :revenue
+    t.integer :established_year
+  end
+  puts "Table '#{TABLE_ENTERPRISES}' created with columns: name, location, revenue, established_year."
+rescue ActiveRecord::StatementInvalid
+  puts "[create_table] Table '#{TABLE_ENTERPRISES}' already exists."
+rescue ActiveRecord::ActiveRecordError => e
+  puts "[create_table] Error occurred while creating table: #{e.message}"
+end
