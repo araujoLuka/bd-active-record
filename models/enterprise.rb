1db4c21867c928d487fa4028ae84be39eef997f7
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2025-05-02 01:26:51 -0300

    chore: improve prints

--- models/enterprise.rb
+++ models/enterprise.rb
@@ -23,7 +23,7 @@ def insert_enterprise(attributes)
 
   enterprise = Enterprise.new(attribute_hash)
   if enterprise.save
-    puts "Enterprise created successfully."
+    puts "Enterprise created successfully: #{enterprise.attributes}"
   else
     puts "Error creating enterprise: #{enterprise.errors.full_messages.join(", ")}"
   end
@@ -39,7 +39,7 @@ def update_enterprise(id, attributes)
   end
 
   if enterprise.update(attribute_hash)
-    puts "Enterprise updated successfully."
+    puts "Enterprise updated successfully: #{enterprise.attributes}"
   else
     puts "Error updating enterprise: #{enterprise.errors.full_messages.join(", ")}"
   end
@@ -52,7 +52,7 @@ end
 def delete_enterprise(id)
   enterprise = Enterprise.find(id)
   if enterprise.destroy
-    puts "Enterprise deleted successfully."
+    puts "Enterprise deleted successfully: #{enterprise.attributes}"
   else
     puts "Error deleting enterprise."
   end
@@ -63,29 +63,22 @@ rescue => e
 end
 
 def list_enterprises
-  enterprises = Enterprise.includes(:projects).all
+  puts "Listing all enterprises..."
+  enterprises = Enterprise.includes(:projects, :employees).all
   if enterprises.any?
     enterprises.each do |enterprise|
-      puts "ID: #{enterprise.id}, Name: #{enterprise.name}, Location: #{enterprise.location}"
-      if enterprise.projects.any?
-        puts "Projects:"
-        enterprise.projects.each do |project|
-          puts "  - Project ID: #{project.id}, Title: #{project.title}, Budget: #{project.budget}"
-        end
-      else
-        puts "  No projects found."
+      puts "  ID: #{enterprise.id}, Name: #{enterprise.name}, Location: #{enterprise.location}"
+      puts "    Projects:"
+      enterprise.projects.each do |project|
+        puts "     |- #{project.id}: #{project.name}, Deadline: #{project.deadline}"
       end
-      if enterprise.employees.any?
-        puts "Employees:"
-        enterprise.employees.each do |employee|
-          puts "  - Employee ID: #{employee.id}, Name: #{employee.name}, Position: #{employee.position}, Salary: #{employee.salary}"
-        end
-      else
-        puts "  No employees found."
+      puts "    Employees:"
+      enterprise.employees.each do |employee|
+        puts "     |- #{employee.id}: #{employee.name}, #{employee.position}, Salary: #{employee.salary}"
       end
     end
   else
-    puts "No enterprises found."
+    puts "  No enterprises found."
   end
 end
 
